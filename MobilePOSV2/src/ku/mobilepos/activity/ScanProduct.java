a32786d1435ff06602bd2a89e90bf763582be009
warrunyou1 <warrunyou1@192.168.182.196>
2013-11-26 16:59:42 +0700

    add new package

---
+++ MobilePOSV2/src/ku/mobilepos/activity/ScanProduct.java
@@ -0,0 +1,22 @@
+/*package ku.mobilepos.activity;
+
+import ku.mobilepos.android.IntentIntegrator;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class ScanProduct implements OnClickListener {
+
+	private InventoryAddMoreItemActivity activity;
+
+	public ScanOnClickListener(InventoryAddMoreItemActivity activity) {
+		this.activity = activity;
+	}
+
+	@Override
+	public void onClick(View v) {
+		IntentIntegrator scanIntegrator = new IntentIntegrator(activity);
+		scanIntegrator.initiateScan();
+	}
+
+}*/
